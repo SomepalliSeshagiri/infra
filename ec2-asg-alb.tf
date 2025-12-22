e2d65fbf0798130a08c558812f2e173e2003acbd
Seshagiri <somepalli.usedu2@gmail.com>
2025-12-22 14:57:38 -0600

    Add Application Load Balancer and Listener

--- ec2-asg-alb.tf
+++ ec2-asg-alb.tf
@@ -148,3 +148,37 @@ resource "aws_autoscaling_group" "app_asg" {
     propagate_at_launch = true
   }
 }
+
+
+# Application Load Balancer
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  load_balancer_type = "application"
+  internal           = false
+
+  security_groups = [
+    aws_security_group.alb_sg.id
+  ]
+
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+
+# ALB Listener (HTTP)
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
