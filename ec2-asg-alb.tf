cb2cf1314d02401a8213849b375ff4d25235f070
Seshagiri <somepalli.usedu2@gmail.com>
2025-12-22 14:15:33 -0600

    Add Launch Template for ASG

--- ec2-asg-alb.tf
+++ ec2-asg-alb.tf
@@ -72,3 +72,48 @@ resource "aws_lb_target_group" "app_tg" {
     Name = "app-tg"
   }
 }
+
+
+# Get the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  owners = ["amazon"]
+}
+
+
+
+
+# Launch Template for EC2 instances
+resource "aws_launch_template" "app_lt" {
+  name_prefix   = "app-lt-"
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  user_data = base64encode(<<EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl enable httpd
+systemctl start httpd
+echo "<h1>Hello from ASG instance $(hostname)</h1>" > /var/www/html/index.html
+EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "asg-instance"
+    }
+  }
+}
