38d206885b56b0ca32f7f22a41f6f671b7f3565b
Seshagiri <somepalli.usedu2@gmail.com>
2025-12-22 14:40:53 -0600

    Add ASG and fixed subnet references

--- ec2-asg-alb.tf
+++ ec2-asg-alb.tf
@@ -117,3 +117,34 @@ EOF
     }
   }
 }
+
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "app_asg" {
+  name                      = "app-asg"
+  max_size                  = 3
+  min_size                  = 1
+  desired_capacity          = 1
+  health_check_type         = "ELB"
+  health_check_grace_period = 60
+
+  vpc_zone_identifier = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.app_lt.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [
+    aws_lb_target_group.app_tg.arn
+  ]
+
+  tag {
+    key                 = "Name"
+    value               = "asg-instance"
+    propagate_at_launch = true
+  }
+}
