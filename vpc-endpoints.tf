63dbf64e304ca8f5d35228d17722fe4365bd5a2d
Seshagiri <somepalli.usedu2@gmail.com>
2025-12-22 12:24:46 -0600

    Add S3 VPC Endpoint to complete VPC layer

---
+++ vpc-endpoints.tf
@@ -0,0 +1,16 @@
+data "aws_region" "current" {}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.current.id}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [
+    aws_route_table.public.id,
+    aws_route_table.private.id
+  ]
+
+  tags = {
+    Name = "s3-endpoint"
+  }
+}
